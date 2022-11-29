b925b0d8594833c36f66f35517b0f424129fe012
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2022-11-29 14:02:52 +0700

    Update V3

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,19 @@
 public class Person {
+
+    /* Contoh Field */
+
    String name;
    String address;
    final String country = "Indonesia";
+
+    /* Contoh Constructor */
+    Person(String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
+   /* Contoh Untuk Menampilkan Method di Console */
+   void sayHello(String paramName){
+       System.out.println("Hello " + paramName + ", My Name is " + name);
+   }
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,15 +1,26 @@
 public class PersonApp {
     public static void main(String[] args) {
 
-        /* Contoh Object */
+        /* Contoh Object dan Manipulation Field */
 
-        var person = new Person();
-        person.name = "Adrian";
-        person.address = "Bandung";
-        //person.country = "Tidak Bisa Diubah";
+        var person1 = new Person("Adrian", "Bandung");
+        person1.name = "Adrian";
+        person1.address = "Bandung";
+        //person.country = "Singapura"; // ERROR
 
-        System.out.println(person.name);
-        System.out.println(person.address);
-        System.out.println(person.country);
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
+        person1.sayHello("Bimo"); // Contoh Method
+
+        Person person2 = new Person("Hernawan", "Cianjut");
+        person2.name = "Hernawan";
+        person2.sayHello("Pratama"); // Contoh Method
+
+        Person person3;
+        person3 = new Person("Herlino", "Jakarta");
+        person3.name = "Herlino";
+        person3.sayHello("Naizar"); // Contoh Method
     }
 }
